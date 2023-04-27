716367b27199a7d2c61fa3db637e47b241b317f6
davidspz1989 <david.spz1989@gmail.com>
2023-04-26 21:19:50 -0600

    se obtiene datos component Perfil de admin

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../service/login/login.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+  user:any=null
+
+  constructor(private readonly loginService:LoginService ){}
+
+
+  ngOnInit(): void {
+    this.user=this.loginService.getUser();
+  }
 
 }
